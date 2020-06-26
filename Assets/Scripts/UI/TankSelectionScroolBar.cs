1c37c7caa282aad85a20e31cddc14a9da9b9d705
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-06-26 03:08:42 +0300

    UI Updates

--- Assets/Scripts/UI/TankSelectionScroolBar.cs
+++ Assets/Scripts/UI/TankSelectionScroolBar.cs
@@ -7,25 +7,27 @@ public class TankSelectionScroolBar : MonoBehaviour
 {
     public RectTransform _transform;
     public RectTransform _firstTransform;
+    public int changeValue = 80;
    
     private void Awake()
     {
         _transform = this.GetComponent<RectTransform>();
+        Debug.Log(_transform.localPosition.y);
     }
     private void Update()
     {
-        if(_transform.localPosition.y >= -75)
+        if(_transform.localPosition.y >= -80)
         {
            // Debug.Log("İlk pozisyon: " + _transform.localPosition.y);
             if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             {
-                _transform.localPosition += new Vector3(0, -85, 0);
+                _transform.localPosition += new Vector3(0, -changeValue, 0);
                 Debug.Log(_transform.localPosition.y);
             }
         }
         else
         {
-            _transform.localPosition = new Vector3(_transform.localPosition.x, 95);
+            _transform.localPosition = new Vector3(_transform.localPosition.x, 122.5f);
         }
         
     }
