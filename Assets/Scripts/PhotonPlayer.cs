8eb1881c719e0944eb922f4a403d84cca40b3a9e
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-07-02 20:47:25 +0300

    SpawnPoints

---
+++ Assets/Scripts/PhotonPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PhotonPlayer : MonoBehaviour
+{
+    private PhotonView _photonView;
+    public GameObject _myTank;
+
+    void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+        int spawnPicker = Random.Range(0, GameManager._gameManager.spawnPoints.Length);
+        if (_photonView.IsMine)
+        {
+            _myTank = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs" , "TankPlayer"), GameManager._gameManager.spawnPoints[spawnPicker].position, GameManager._gameManager.spawnPoints[spawnPicker].rotation, 0);
+        }
+    }
+}
